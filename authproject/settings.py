d9d6cea9f4623023abe7c35c89a8553735a56e57
dunice-romanov <artem.romanov@dunice.ru>
2017-03-06 23:13:16 +0300

    set token expire to 1 hour

--- authproject/settings.py
+++ authproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,5 +136,6 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3600),
     #'JWT_RESPONSE_PAYLOAD_HANDLER': 'auth_app.refresh.jwt_response_payload_handler',
 }
